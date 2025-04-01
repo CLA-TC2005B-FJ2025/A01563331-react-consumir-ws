32db171e3ff36c2b5e6068cade0f23600213a68c
Mauricio <152633503+mauriciobalbuena@users.noreply.github.com>
2025-04-01 16:35:41 +0000

    tuki

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const getUsuario = async (user_id) => {
 
-    const response = await axios.get('http://localhost:2025/test/' + user_id, {
+    const response = await axios.get('https://urban-space-guacamole-69959wg5rgrjf5g5v-2025.app.github.dev/test/' + user_id, {
     });
     
     console.log(response)
